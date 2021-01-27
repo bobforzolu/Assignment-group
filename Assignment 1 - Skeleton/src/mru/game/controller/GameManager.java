687c87ebfa076a76187debebd2eb80fb64e31c29
Frohjur <69992260+bobforzolu@users.noreply.github.com>
2021-01-26 19:25:29 -0700

    game manager changes

--- Assignment 1 - Skeleton/src/mru/game/controller/GameManager.java
+++ Assignment 1 - Skeleton/src/mru/game/controller/GameManager.java
@@ -1,8 +1,9 @@
 package mru.game.controller;
-
+import mru.game.model.*;
+import java.util.*;
 
 public class GameManager {
-	
+	ArrayList<Player> players = new ArrayList<Player>();
 	/* In this class toy'll need these methods:
 	 * A constructor
 	 * A method to load the txt file into an arraylist (if it exists, so you check if the txt file exists first)
@@ -11,7 +12,48 @@ public class GameManager {
 	 * A method to find the top players
 	 * Depending on your designing technique you may need and you can add more methods here 
 	 */
-
+	public GameManager()
+	{
+		
+	}
+	private void loadFile()
+	{
+		
+	}
+	public void topPlayer()
+	{
+		
+	}
+	public void findPlayer()
+	{
+		
+	}
+	private void saveFile()
+	{
+		
+	}
+	private int search()
+	{
+		// check if an item is in the list or not
+		int foundInfo = -1;
+		// current location in the list
+		int j = 0 ;
+		// list size
+		int dataSize;
+		// player class
+		Player found;
+		
+		
+		dataSize = players.size();
+		
+		
+		
+		return foundInfo;
+		
+	}
+	
+	
+	
 	
 
 }
