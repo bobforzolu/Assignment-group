7465bb51765e01a45e75dbbe28fc0cfd5c88404f
Frohjur <69992260+bobforzolu@users.noreply.github.com>
2021-02-05 13:23:44 -0700

    changes

--- Assignment 1 - Skeleton/src/mru/game/controller/PuntoBancoGame.java
+++ Assignment 1 - Skeleton/src/mru/game/controller/PuntoBancoGame.java
@@ -1,6 +1,11 @@
 package mru.game.controller;
+import mru.game.view.*;
 
-import java.util.Random;
+import java.io.FileNotFoundException;
+
+import mru.game.model.*;
+import java.util.*;
+
 
 public class PuntoBancoGame {
 	
@@ -11,42 +16,99 @@ public class PuntoBancoGame {
 	 */
 	CardDeck cardDeck;
 	Random rdm;
+	AppMenu appMenu;
+	Player player;
+	GameManager gm;
+	
+	private double bettAmount;
+	
+	
+	public double getBett()
+	{
+		return bettAmount;
+	}
+	
 	public PuntoBancoGame() 
 	{
 		 cardDeck = new CardDeck();
 		 rdm = new Random();
-		 launchGame();
+		 appMenu = new AppMenu();
+		 
+		 player = new Player();
+		 
 	}
 	
-	public void launchGame() 
+	public boolean launchGame() 
 	{
+		// score
+		int playerScore = 0;
+		int brokerScore = 0;
+		int tieScore = 0;
 		
+		// players betting option
+		char choice = appMenu.betMenu();
+		char playerBet = 'p';
+		char brokerBet = 'b';
+		char tie = 't';
+		double bettAmount = appMenu.placeBet();
 		
+		boolean hasWon = false;
+			
 		for(int i=0; i<=2; i++)
 		{
-		int playercard = rdm.nextInt(12) +1;
-		int bankercard = rdm.nextInt(12) +1;
-		
-		Card player = cardDeck.getDeck().get(playercard);
-		Card banker = cardDeck.getDeck().get(bankercard);
 			
-		System.out.println(banker);
-		System.out.println(player);
-
-		if(player.getRank() > banker.getRank())
-		{
-			System.out.println("player gets a point");
-
-		}else if(player.getRank() < banker.getRank())
-		{
-			System.out.println("banker gets a point");
-
-		}else
-		{
-			System.out.print("hk");
-		}
+			int playercard = rdm.nextInt(12) +1;
+			int bankercard = rdm.nextInt(12) +1;
 			
+			
+			Card playerHand = cardDeck.getDeck().get(playercard);
+			Card bankerHand = cardDeck.getDeck().get(bankercard);
+			
+			// we need a menu 
+			System.out.println(bankerHand);
+			System.out.println(playerHand);
+	
+			if(playerHand.getRank() > bankerHand.getRank())
+			{
+				playerScore++;
+				System.out.println("player: " + playerScore);
+
+				if(playerScore == 2)	
+				{
+					if(choice == playerBet)
+					{
+						hasWon = true;
+					}
+					break;
+				}
+			}else if(playerHand.getRank() < bankerHand.getRank())
+			{
+				brokerScore++;
+				System.out.println("broker: "+brokerScore);
+
+				if(brokerScore == 2)
+				{
+					if(choice == brokerBet)
+					{	
+						hasWon = true;
+					}
+					break;
+				}
+			}else {
+				tieScore ++;
+				if(playerScore == 1 && tieScore == 1 && brokerScore == 1)
+				{
+					if(choice == tie)
+					{	
+						hasWon = true;
+					}
+					
+				}
+			}
 		}
-		
+		return hasWon;
 	}
+	
+	
+
 }
