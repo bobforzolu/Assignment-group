ad83b44d0ce329052e4b0525253bb0ef9d2f0288
Frohjur <69992260+bobforzolu@users.noreply.github.com>
2021-02-11 20:59:17 -0700

    added a test unit

---
+++ Assignment 1 - Skeleton/test/mru/test/driver/CardDeck.java
@@ -0,0 +1,14 @@
+package mru.test.driver;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CardDeck {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
