b78defd316b1f3a4574a171a1eea417bf78cc4c0
Frohjur <69992260+bobforzolu@users.noreply.github.com>
2021-02-11 22:28:09 -0700

    test 1 to 3 is done

--- Assignment 1 - Skeleton/test/mru/test/driver/Cardtest.java
+++ Assignment 1 - Skeleton/test/mru/test/driver/Cardtest.java
@@ -2,18 +2,53 @@ package mru.test.driver;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import mru.game.controller.Card;
 import mru.game.controller.CardDeck;
 
 class Cardtest {
-
+	private CardDeck card;
+	
+	@BeforeEach
+	public void getDeck()
+	{
+		 card = new CardDeck();
+	}
 	@Test
-	void test() {
-		CardDeck card = new CardDeck();
-		System.out.println(card.getDeck().get(0).getSuit());
-		System.out.println(card.getDeck().get(0).getRank());
+	void getCardInfo() {
+		
+		System.out.println(card.getDeck().get(0));
+		System.out.println("Suit: "+card.getDeck().get(0).getSuit());
+		System.out.println("Rank: "+card.getDeck().get(0).getRank());	
+	}
+	@Test
+	/**
+	 * checks weather the numeric values of suits and values of the rank match
+	 * the to their expected stings
+	 */
+	void cardtest() {
+		
+		if(card.getDeck().get(0).getRank() < 10)
+		{
+			int rank = card.getDeck().get(0).getRank();
+			String name = card.getDeck().get(0).getSuit();
+			String cardInfo = rank + " of " + name;
+			String expectedcars = card.getDeck().get(0).toString();
+		
+			assertEquals(expectedcars, cardInfo);
 
+		}else {
+			int rank = card.getDeck().get(0).getRank();
+			String name = card.getDeck().get(0).getSuit();
+			String cardInfo = name + " of " +rank ;
+			String expectedcars = card.getDeck().get(0).toString();
+		
+			assertEquals(expectedcars, cardInfo);
+		}
+		
+		
 		
 	}
 
